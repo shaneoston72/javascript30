15e501c4a77d9a396c9b34f4e75bc9385442ff42
Shane Oston Stowe <shane@shaneoston.uk>
2017-04-22 12:50:08 +0100

    Initial commit
    Consolidated all katas into one folder

---
+++ holdShiftCheckboxes/app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by shane on 4/17/17.
+ */
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+function handleCheck(e) {
+    let inBetween = false;
+
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if(checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+
+    lastChecked = this;
+};
---
+++ javascriptDrumKit/README.md
@@ -0,0 +1,10 @@
+# JavaScript Drum Kit
+This is a simple vanilla JavaScript app.  The app demonstrates the following patterns:
+
+* adding listeners for `keydown` events;
+* key events (`keydown`);
+* playing audio on `keydown`;
+* adding a class on `keydown`;
+* listening for the `transitionend` event to remove classes.
+
+Completed as part of Wes Bos's [JavaScript30 course](https://javascript30.com).
